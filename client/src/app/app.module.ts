5c84414dbab45129f6273e9a65aa36aece76b4f9
Jeet Prakash <11135910+jeetprksh@users.noreply.github.com>
2025-12-30 17:23:29 +0530

    UI: Removed Angular Material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatGridListModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
